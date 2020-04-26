71d2878633256dea7d510598b2a833197ddee932
Manu Vishwanath <Manu.Vishwanath@ametek.com>
2020-04-26 21:26:15 +0530

    Updates

---
+++ precache-manifest.196b87bc05d1a4316d2e0ed6bd05a53d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da41ddac2ad965a50192030609e4e74f",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "6027c64154f8c3525436",
+    "url": "/monsters/static/css/main.53d527ec.chunk.css"
+  },
+  {
+    "revision": "9f834d1fc4df12fb31cd",
+    "url": "/monsters/static/js/2.308474c0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters/static/js/2.308474c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6027c64154f8c3525436",
+    "url": "/monsters/static/js/main.70c5708a.chunk.js"
+  },
+  {
+    "revision": "64e2ee1a6a4fdc686046",
+    "url": "/monsters/static/js/runtime-main.e75998aa.js"
+  }
+]);
